28dc411476cb1e2be5fe925e395011e212296016
alexandretec77@gmail.com <alexandretec77@gmail.com>
2021-11-29 17:24:38 -0300

    Implementando arquivo

---
+++ logica-de-programacao/estrutura-condicional/curso_programacao/src/fixation/exercise/Exercise7.java
@@ -0,0 +1,56 @@
+/* Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+situação.
+
+Data atual: 29/11/2021
+
+Autor (a): Alexandre Gonçalo
+
+*/
+
+package fixation.exercise;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise7 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in);
+        String mensagem = "";
+
+            System.out.println("==============================");
+            System.out.println("       PLANO CARTESIANO       ");
+            System.out.println("==============================");
+            System.out.print("Coordenada X: ");
+                final double xAxis = sc.nextDouble();
+            System.out.print("Coordenada Y: ");
+                final double yAxis = sc.nextDouble();
+            System.out.println("==============================");
+                if (xAxis > 0.0 && yAxis > 0.0) {
+                    mensagem = "Q1";
+                } else if (xAxis < 0.0 && yAxis > 0.0) {
+                    mensagem = "Q2";
+                } else if (xAxis < 0.0 && yAxis < 0.0) {
+                    mensagem = "Q3";
+                } else if (xAxis > 0.0 && yAxis < 0.0) {
+                    mensagem = "Q4";
+                } else {
+                    mensagem = "Origem";
+                }
+
+            System.out.println("          RESULTADO           ");
+            System.out.println("==============================");
+            System.out.printf("%s", mensagem);
+
+        sc.close();
+
+    }
+}
