f7d487db1292230eaf012fdb0b068492c479443f
dev-alexandre17 <alexandretec77@gmail.com>
2022-04-27 22:22:56 -0700

    Adicionando códigos e anotações em exercise-1 de vetores

--- programacao-orientada-a-objetos/vetores/exercise-1/curso_programacao/src/application/Program.java
+++ programacao-orientada-a-objetos/vetores/exercise-1/curso_programacao/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import entities.Rent;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Program {
 
@@ -16,17 +17,21 @@ public class Program {
 
         double[] vect = new double[9];
 
-        for (int i = 0; i < rooms; i++) {
-            sc.nextLine();
+        for (int i = 1; i < rooms + 1; i++) {
+            System.out.println();
             System.out.printf("Rent #%d%n", i);
             System.out.print("Name: ");
             String name = sc.nextLine();
-            sc.nextLine();
+            sc.next();
             System.out.print("Email: ");
             String email = sc.nextLine();
+            sc.nextLine();
             System.out.print("Room: ");
             int room = sc.nextInt();
         }
+
+        // Vetor para armazenar nome e email
+        // Método sort para ordenar informações
 
         sc.close();
 
