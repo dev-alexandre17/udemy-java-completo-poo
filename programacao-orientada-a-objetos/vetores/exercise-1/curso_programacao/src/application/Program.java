5f1e96535b3303f652c36a9b1a1bba1bcb3c2dc7
dev-alexandre17 <alexandretec77@gmail.com>
2022-05-08 16:36:21 -0800

    Adicionando métodos finais para imprimir informações

--- programacao-orientada-a-objetos/vetores/exercise-1/curso_programacao/src/application/Program.java
+++ programacao-orientada-a-objetos/vetores/exercise-1/curso_programacao/src/application/Program.java
@@ -1,37 +1,40 @@
 package application;
 
 import entities.Rent;
-
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Rent customer;
+
+        Rent[] roomNumbers = new Rent[10];
 
         System.out.print("How many rooms will be rented? ");
         int rooms = sc.nextInt();
 
-        double[] vect = new double[9];
-
-        for (int i = 1; i < rooms + 1; i++) {
+        for (int i = 1; i <= rooms; i++) {
             System.out.println();
             System.out.printf("Rent #%d%n", i);
             System.out.print("Name: ");
+            sc.nextLine();
             String name = sc.nextLine();
-            sc.next();
             System.out.print("Email: ");
             String email = sc.nextLine();
-            sc.nextLine();
             System.out.print("Room: ");
             int room = sc.nextInt();
+            roomNumbers[room] = new Rent(name, email);
         }
 
-        // Vetor para armazenar nome e email
-        // Método sort para ordenar informações
+        System.out.println();
+        System.out.println("Busy rooms: ");
+
+        for (int y = 0; y < roomNumbers.length; y++) {
+            if (roomNumbers[y] != null) {
+                System.out.println(y + ": " + roomNumbers[y]);
+            }
+        }
 
         sc.close();
 
