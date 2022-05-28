779a522d413d83d2a656f5db30c0d35c7775cb7c
dev-alexandre17 <alexandretec77@gmail.com>
2022-05-27 22:27:53 -0700

    Interligação de classe com lista

--- programacao-orientada-a-objetos/listas/exercise-1/curso_programacao/src/application/Program.java
+++ programacao-orientada-a-objetos/listas/exercise-1/curso_programacao/src/application/Program.java
@@ -11,19 +11,19 @@ public class Program {
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-
-        Locale.setDefault(Locale.US);
+        List<Employee> list = new ArrayList<Employee>();
 
         Employee employee;
+
+        Locale.setDefault(Locale.US);
 
         Scanner sc = new Scanner(System.in);
 
             System.out.print("How many employees will be registered? ");
             int employees = sc.nextInt();
+            System.out.println();
 
             for (int i = 1; i <= employees; i++) {
-                System.out.println();
                 System.out.println("Employee #" + i + ":");
                 System.out.print("Id: ");
                 int id = sc.nextInt();
@@ -32,6 +32,9 @@ public class Program {
                 sc.nextLine();
                 System.out.print("Salary: ");
                 double salary = sc.nextDouble();
+                System.out.println();
+                employee = new Employee(id, name, salary);
+                list.add(employee);
             }
 
             System.out.print("Enter the employee id that will have salary increase: ");
@@ -39,6 +42,8 @@ public class Program {
 
             System.out.print("Enter the percentage: ");
             double percentage = sc.nextDouble();
+
+            System.out.println();
 
         sc.close();
 
--- programacao-orientada-a-objetos/listas/exercise-1/curso_programacao/src/entities/Employee.java
+++ programacao-orientada-a-objetos/listas/exercise-1/curso_programacao/src/entities/Employee.java
@@ -33,4 +33,8 @@ public class Employee {
         this.salary = salary;
     }
 
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + salary;
+    }
 }
